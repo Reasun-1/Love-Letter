cb3c98cb9734f8da64d0b2b970c9b38b1bc58be9
xhenetak <krasniqi@cip.ifi.lmu.de>
2021-05-07 02:58:44 +0200

    ChatAndGameWindow edited/improved + all fx:id's added

---
+++ src/client/viewmodel/ChatAndGameWindowController.java
@@ -0,0 +1,14 @@
+package client.viewmodel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import client.controller.Client;
+
+// Controller Class for the Chat and Game Window connected with the fxml file via data binding
+
+public class ChatAndGameWindowController {
+
+}
