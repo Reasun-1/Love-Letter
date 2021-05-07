2d5cbbaaa3d56ce82bbb478bab6947a2822039d4
xhenetak <krasniqi@cip.ifi.lmu.de>
2021-05-07 03:20:35 +0200

    ChatAndGameWindow Controller angefangen, noch in Bearbeitung:)

--- src/client/viewmodel/ChatAndGameController.java
+++ src/client/viewmodel/ChatAndGameController.java
@@ -1,115 +1,195 @@
 package client.viewmodel;
 
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import client.controller.Client;
 
-// Controller Class for the Chat Window connected with the fxml file via data binding
+// Controller Class for the Chat and Game Window connected with the fxml file via data binding
 
 public class ChatAndGameController {
-
-    @FXML
-    private MenuItem drawnCardItem;
-
-    @FXML
-    private MenuItem handCardItem;
-
-    @FXML
-    private MenuButton playCardButton;
 
     @FXML
     private AnchorPane chatRootPane;
 
     @FXML
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private TextField clientName;
+
+    @FXML
+    private SplitPane mainSplitPane;
+
+    @FXML
     private AnchorPane writePane;
+
+    @FXML
+    private TextField messageField;
+
+    @FXML
+    private ComboBox<?> sendToBox;
 
     @FXML
     private Button sendButton;
 
     @FXML
-    private TextField typeField;
-
-    @FXML
     private AnchorPane chatFieldPane;
 
     @FXML
-    private TextArea chatView;
+    private ScrollPane scrollField;
 
     @FXML
-    private AnchorPane gameControlPane;
+    private AnchorPane gameFieldPane;
 
     @FXML
-    private AnchorPane playCardPane;
+    private AnchorPane scorePane;
 
     @FXML
-    private TextArea handCards;
+    private ImageView imageToken;
 
     @FXML
-    private Button playButton;
+    private Text scoreText;
 
     @FXML
-    private Button createGame;
+    private TextField personalScore;
 
     @FXML
-    private Button joinGame;
+    private TextField secPlayerScore;
 
     @FXML
-    private Button startGame;
+    private TextField thirdPlayerScore;
+
+    @FXML
+    private TextField fourthPlayerScore;
+
+    @FXML
+    private Button startGameButton;
+
+    @FXML
+    private GridPane mainGridPane;
+
+    @FXML
+    private AnchorPane secPlayerPane;
+
+    @FXML
+    private Text secPlayerText;
+
+    @FXML
+    private AnchorPane secHandCardPane;
+
+    @FXML
+    private MenuButton secHandCardMenuButton;
+
+    @FXML
+    private MenuItem secHandCardItemOne;
+
+    @FXML
+    private MenuItem secHandCardItemTwo;
+
+    @FXML
+    private AnchorPane secPlayerMenuPane;
+
+    @FXML
+    private MenuButton secDrawnCardMenuButton;
+
+    @FXML
+    private MenuItem secDrawnCardItemOne;
+
+    @FXML
+    private MenuItem secDrawnCardItemTwo;
+
+    @FXML
+    private AnchorPane thirdPlayerPane;
+
+    @FXML
+    private Text thirdPlayerText;
+
+    @FXML
+    private MenuButton thirdHandCardMenuButton;
+
+    @FXML
+    private MenuItem thirdHandCardItemOne;
+
+    @FXML
+    private MenuItem thirdHandCardItemTwo;
+
+    @FXML
+    private MenuButton thirdDrawnCardMenuButton;
+
+    @FXML
+    private MenuItem thirdDrawnCardItemOne;
+
+    @FXML
+    private MenuItem thirdDrawnCardItemTwo;
+
+    @FXML
+    private AnchorPane fourthPlayerPane;
+
+    @FXML
+    private Text fourthPlayerText;
+
+    @FXML
+    private AnchorPane fourthPlayerButtonPane;
+
+    @FXML
+    private MenuButton fourthHandCardMenuButton;
+
+    @FXML
+    private MenuItem fourthHandCardItemOne;
+
+    @FXML
+    private MenuItem fourthHandCardItemTwo;
+
+    @FXML
+    private MenuButton fourthDrawnCardMenuButton;
+
+    @FXML
+    private MenuItem fourthDrawnCardItemOne;
+
+    @FXML
+    private MenuItem fourthDrawnCardItemTwo;
+
+    @FXML
+    private AnchorPane youPane;
+
+    @FXML
+    private MenuButton yourHandCardMenuButton;
+
+    @FXML
+    private MenuItem yourHandCardItemOne;
+
+    @FXML
+    private MenuItem yourHandCardItemTwo;
+
+    @FXML
+    private MenuButton yourDrawnCardMenuButton;
+
+    @FXML
+    private MenuItem yourDrawnCardItemOne;
+
+    @FXML
+    private MenuItem yourDrawnCardItemTwo;
+
+    @FXML
+    private AnchorPane yourPane;
+
+    @FXML
+    private Text youText;
 
     private Client client;
-
-    //  Method for the initialization of the Button and the Text Field via bidirectional binding.
-    public void init(Client client) {
-        this.client = client;
-        /*typeField.textProperty().bindBidirectional(chatVM.getMessageOutput());
-        chatView.textProperty().bindBidirectional(chatVM.getChatInput());
-        handCards.textProperty().bindBidirectional(chatVM.getHandCards());
-        handCardItem.textProperty().bindBidirectional(chatVM.getHandCardItem());
-        drawnCardItem.textProperty().bindBidirectional(chatVM.getDrawnCardItem());*/
-        //sendButton.defaultButtonProperty().bindBidirectional(ViewModel.sendButtonProperty());
-    }
-
-    // Method for sending messages to the server.
-    @FXML
-    private void sendButton(ActionEvent event) {
-        client.sendMessage(typeField.getText());
-    }
-
-    @FXML
-    public void createGame(ActionEvent actionEvent) {
-        client.createGame();
-    }
-
-    @FXML
-    public void joinGame(ActionEvent actionEvent) {
-        client.joinGame();
-    }
-
-    @FXML
-    public void startGame(ActionEvent actionEvent) {
-        client.startGame();
-    }
-
-    @FXML
-    public void playHandCard(ActionEvent actionEvent) {
-        client.playHandCard();
-    }
-
-    @FXML
-    public void playDrawnCard(ActionEvent actionEvent){
-        client.playDrawnCard();
-    }
-
-
-    // Method for setting Client messages in the List View.
-
-    /*public <Client> void setClient(Client client) {
-
-        //ViewModel.setClient(client);
-        //chatView.setItems(ViewModel.getClient().chatMessages);
-    }*/
 
 }
