0851b950985801c33fc0e8047b70c04118a512ec
ren <ren@cip.ifi.lmu.de>
2021-08-05 16:51:09 +0200

    ff

--- src/client/controller/Client.java
+++ src/client/controller/Client.java
@@ -143,6 +143,7 @@ public class Client extends Application {
 
         // Always connect to localhost and fixed port (maybe ask for ip and port?)
         socket = new Socket("127.0.0.1", 5200);
+        //socket = new Socket("sep21.dbs.ifi.lmu.de", 52018);
 
         // Create writer to send messages to server via the TCP-socket
         OUT = new PrintWriter(socket.getOutputStream(), true);
